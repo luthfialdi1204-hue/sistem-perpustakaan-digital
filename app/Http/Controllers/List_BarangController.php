6544a504c6ccaad6e558ed8469627078da0d4bd7
luthfialdi1204-hue <luthfialdi1204@gmail.com>
2026-03-28 03:47:07 -0700

    tugas pratikum

---
+++ app/Http/Controllers/List_BarangController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class List_BarangController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-Use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ListBarangController;
+use App\Http\Controllers\LoginController;
 
-// Route::get('/', function () {
-   // return view('welcome');
-//});
+Route::prefix('admin')->group(function () {
+   Route::get('/dashboard', function () {
+      return 'Admin Dashboard';
+   });
 
-Route::get('/', [HomeController::class, 'index']); 
-Route::get('/contact', [HomeController::class, 'contact']);
+   Route::get('/users', function () {
+      return 'Admin Users';
+   });
+});
+
+Route::get('/listbarang/{id}/{nama}', function($id, $nama){ 
+  return view('list_barang', compact('id', 'nama'));
+});
+
+Route::get('/login', function (){
+   return view('login');
+});
+
+Route::get('/dashboard', function (){
+   return view('dashboard');
+});
+
+Route::get('/list_barang', function (){
+   return view('list_barang');
+});
